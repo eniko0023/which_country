f5daa75e7997a6989b4762248becb3057a35b88e
enc <kisbenedekeniko01@gmail.com>
2025-01-08 09:39:22 +0100

    veriosn 1.1 jan8

--- app.js
+++ app.js
@@ -14,28 +14,51 @@ async function sendRequest() {
         console.log(data);
         const flags = getFlags(data);
         renderFlags(flags);
-        // console.log(flags);
         const countryNames = getNames(data);
+        startButtonManager(countryNames, data);
     } catch(error){
         console.log(error.message);
     }
 }
 
+//zaszlokepek listajat elkesziti
 function getFlags(data){
     const flags = data.map(item => item.flags.png)
     return flags;
 }
 
+//orszagok neveinek listaja
 function getNames(data){
     const countries = data.map(({ name }) => name.common)
     return countries;
 }
 
+//zaszlok renderelese
 function renderFlags(flags) {
-    const container = document.querySelector(".flags")
-    container.innerHTML = ""
+    const container = document.querySelector(".flags");
+    container.innerHTML = "";
     const content = flags.map(flag => `
         <div><img src="${flag}"></div>
         `).join("");
-    container.innerHTML = content
+    container.innerHTML = content;
+}
+
+//getrandomname
+function getRandomName(nameList){
+    const countryName = nameList.sort(() => Math.random() - 0.5).splice(0, 1);
+    return countryName;
+}
+
+//a kitalalndo orszag nevet sorsoljuk
+function showRandomName(name){
+    const input = document.querySelector("[type='text']");
+    if (input instanceof HTMLInputElement){
+        input.value = "";
+        input.value = name;
+    }
+}
+
+//flags manager
+function flagsManager(nameList, data){
+    
 }
